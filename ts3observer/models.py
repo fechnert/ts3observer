037d2631fcaea8f9cf7e120887cdd6da02bbad10
Tim Fechner <tim.b.f@gmx.de>
2014-12-04 18:57:57 +0100

    Improve client representation

--- ts3observer/models.py
+++ ts3observer/models.py
@@ -19,7 +19,7 @@ class Client(object):
             setattr(self, key, value)
 
     def __repr__(self):
-        return '<Client object ({}: {})>'.format(self.clid, self.client_nickname)
+        return '<Client [{}]>'.format(Escaper.decode(self.client_nickname))
 
     @Escaper.encode_attr('reason')
     def kick(self, featurename, reasonid, reason='ByeBye', **kwargs):
